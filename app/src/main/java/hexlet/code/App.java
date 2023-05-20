f881eb0dc3682764ee60f0c38b8ec98f13a9846d
Nadia Makeeva <lossa@inbox.ru>
2023-05-20 22:01:28 +0300

    update 3 files & add 3 files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,33 +5,48 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",
         description = "Compares two configuration files and shows a difference.")
 
-public class App {
-        @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-        boolean version;
+public class App implements Callable {
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+   boolean version;
 
-        @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-        boolean help;
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+   boolean help;
 
-        @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
-        File format;
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+   File format;
 
-        @Parameters(index = "0", description = "path to first file", defaultValue = "/etc/hosts")
-        private File filepath1 = new File("/etc/hosts");
+    @Parameters(index = "0", description = "path to first file", defaultValue = "/home/nadi777/file1.json")
+   private static String filepath1;
 
-        @Parameters(index = "0", description = "path to second file", defaultValue = "/etc/hosts")
-        private File filepath2 = new File("/etc/hosts");
+    @Parameters(index = "1", description = "path to second file", defaultValue = "/home/nadi777/file2.json")
+   private static String filepath2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
-        App app = CommandLine.populateCommand(new App(), args);
-        if (app.help) {
-            CommandLine.usage(new App(), System.out);
-            return;
-        }
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Object call() throws Exception {
+        //System.out.println(Differ.getData(getFilepath1()));
+        //System.out.println(Differ.getData(getFilepath1()));
+        System.out.println(Differ.toString
+                (Differ.generate(Differ.getData(getFilepath1()), Differ.getData(getFilepath2()))));
+        return 0;
+    }
+
+    public static String getFilepath1() {
+        return filepath1;
+    }
+
+    public static String getFilepath2() {
+        return filepath2;
     }
 }
 
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,61 @@
 package hexlet.code;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
+
 
 public class Differ {
-    public static String generate() {
-        return "nothing";
+
+    public static Map getData(String filename) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map
+                = objectMapper.readValue(new File(filename), new TypeReference<Map<String, Object>>() { });
+        return map;
+    }
+
+    public static Map<String, Object> generate(Map<String, Object> file1, Map<String, Object> file2) {
+        Map<String, Object> result = new LinkedHashMap<>();
+
+        Set<String> unionKeys = new HashSet<>();
+        unionKeys.addAll(file1.keySet());
+        unionKeys.addAll(file2.keySet());
+        unionKeys = unionKeys.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        for (String key: unionKeys) {
+            if (file1.keySet().contains(key) && file1.get(key).equals(file2.get(key))) {
+                result.put("  " + key, file1.get(key));
+            } else if (file1.keySet().contains(key) && !file2.keySet().contains(key)) {
+                result.put("- " + key, file1.get(key));
+            } else if (file2.keySet().contains(key) && !file1.keySet().contains(key)) {
+                result.put("+ " + key, file2.get(key));
+            } else if (file1.keySet().contains(key) && file2.keySet().contains(key)) {
+                result.put("- " + key, file1.get(key));
+                result.put("+ " + key, file2.get(key));
+            }
+        }
+        return result;
+    }
+
+    public static String toString(Map<String, Object> hmap) {
+        String all = "";
+        if (hmap.isEmpty()) {
+            return "{}";
+        }
+        for (Map.Entry<String, Object> entry: hmap.entrySet()) {
+            String str = "  " + entry.getKey() + ": "  +  entry.getValue() + '\n';
+            all = all.concat(str);
+        }
+        return "{\n" + all + "}";
     }
 }
