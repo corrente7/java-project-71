633fbfe59b41c5abd9be2dbf2ef4565856907f99
Nadia Makeeva <lossa@inbox.ru>
2023-06-02 22:04:45 +0300

    update 1 file

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -32,22 +32,7 @@ public class App implements Callable {
 
     @Override
     public Object call() throws Exception {
-//        System.out.println(Parser.detectTypeFile(getFilepath1()));
-//        System.out.println(Parser.detectTypeFile(getFilepath2()));
         System.out.println(Differ.generate(getFilepath1(), getFilepath2(), format));
-//        Map<String, Object> file1 = Map.of(
-//                "date_end", "2020-06-10T23:59:59+03:00",
-//                "description", "product1",
-//                "discount_label", "1+1",
-//                "id", 11111
-//        );
-//        Map<String, Object> file2 = Map.of(
-//                "barcode", "3113097501031",
-//                "description", "product2",
-//                "discount_label", "1+1",
-//                "price_is_from", false
-//        );
-//        System.out.println(Differ.generate(file1, file2));
         return 0;
     }
 
