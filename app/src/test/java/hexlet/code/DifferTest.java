bd1a4e7383870e9c5a6085ce6e3800127438cf6a
Nadia Makeeva <lossa@inbox.ru>
2023-06-09 19:57:39 +0300

    update 1 file

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -6,11 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static hexlet.code.Differ.generate;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -32,7 +29,7 @@ public class DifferTest {
         filepathInput1 = absolutePath + "/test1.json";
         filepathInput2 = absolutePath + "/test2.json";
         String filepath3 = absolutePath + "/test_json_plain";
-        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");;
+        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");
 
         assertEquals(expected, Differ.generate(filepathInput1, filepathInput2, "plain"));
 
@@ -43,7 +40,7 @@ public class DifferTest {
         filepathInput1 = absolutePath + "/test1.json";
         filepathInput2 = absolutePath + "/test2.json";
         String filepath3 = absolutePath + "/test_json_stylish";
-        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");;
+        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");
 
         assertEquals(expected, Differ.generate(filepathInput1, filepathInput2, "stylish"));
         //assertThat(generate(filepathInput1, filepathInput2, format)).isEqualTo(expected);
@@ -54,7 +51,7 @@ public class DifferTest {
         filepathInput1 = absolutePath + "/test1.json";
         filepathInput2 = absolutePath + "/test2.json";
         String filepath3 = absolutePath + "/test_json_json";
-        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");;
+        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");
 
         assertEquals(expected, Differ.generate(filepathInput1, filepathInput2, "json"));
         //assertThat(generate(filepathInput1, filepathInput2, format)).isEqualTo(expected);
@@ -65,13 +62,13 @@ public class DifferTest {
         filepathInput1 = absolutePath + "/test1.json";
         filepathInput2 = absolutePath + "/test2.json";
         String filepath3 = absolutePath + "/test_json_stylish";
-        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");;
+        String expected = Files.readString(Paths.get(filepath3)).replaceAll("\r\n", "\n");
 
         assertEquals(expected, Differ.generate(filepathInput1, filepathInput2));
         //assertThat(generate(filepathInput1, filepathInput2, format)).isEqualTo(expected);
     }
 
-       @Test
+    @Test
     public void generateYamlPlainTest() throws Exception {
         filepathInput1 = absolutePath + "/test3.yaml";
         filepathInput2 = absolutePath + "/test4.yaml";
