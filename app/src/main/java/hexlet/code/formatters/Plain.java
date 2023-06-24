877e2d852f8d57b41f6f18c5e705cc2c28e9c35a
Nadia Makeeva <lossa@inbox.ru>
2023-06-25 00:27:10 +0300

    update 3 files

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -8,18 +8,20 @@ public class Plain {
     public static String formatToPlain(List<Map<String, Object>> list) {
         StringBuilder strResult = new StringBuilder();
         for (Map<String, Object> map: list) {
-            switch ((String)(map.get("status"))) {
+            switch ((String) (map.get("status"))) {
                 case("updated"):
-                strResult.append("Property '" + map.get("field") + "' was updated. "
+                    strResult.append("Property '" + map.get("field") + "' was updated. "
                         + "From " + convertObjectToString(map.get("old value")) + " to "
                         + convertObjectToString(map.get("new value")) + '\n');
                     break;
                 case("removed"):
-                strResult.append("Property '" + map.get("field") + "' was removed" + '\n');
+                    strResult.append("Property '" + map.get("field") + "' was removed" + '\n');
                     break;
                 case("added"):
-                strResult.append("Property '" + map.get("field")
+                    strResult.append("Property '" + map.get("field")
                         + "' was added with value: " + convertObjectToString(map.get("new value")) + '\n');
+                    break;
+                default:
                     break;
             }
         }
--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -8,19 +8,21 @@ public class Stylish {
     public static String formatToStylish(List<Map<String, Object>> list) {
         StringBuilder strResult = new StringBuilder();
         for (Map<String, Object> map: list) {
-            switch ((String)(map.get("status"))) {
+            switch ((String) (map.get("status"))) {
                 case ("same"):
-                strResult.append("  " + "  " + map.get("field") + ": " + map.get("old value") + '\n');
+                    strResult.append("  " + "  " + map.get("field") + ": " + map.get("old value") + '\n');
                     break;
                 case ("removed"):
-                strResult.append("  " + "- " + map.get("field") + ": " + map.get("old value") + '\n');
+                    strResult.append("  " + "- " + map.get("field") + ": " + map.get("old value") + '\n');
                     break;
                 case ("added"):
-                strResult.append("  " + "+ " + map.get("field") + ": " + map.get("new value") + '\n');
+                    strResult.append("  " + "+ " + map.get("field") + ": " + map.get("new value") + '\n');
                     break;
                 case ("updated"):
-                strResult.append("  " + "- " + map.get("field") + ": " + map.get("old value") + '\n');
-                strResult.append("  " + "+ " + map.get("field") + ": " + map.get("new value") + '\n');
+                    strResult.append("  " + "- " + map.get("field") + ": " + map.get("old value") + '\n');
+                    strResult.append("  " + "+ " + map.get("field") + ": " + map.get("new value") + '\n');
+                    break;
+                default:
                     break;
             }
         }
