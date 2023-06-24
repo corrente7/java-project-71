7d3a69b238c3a9cffbac7b224f9ed72fc17989be
Nadia Makeeva <lossa@inbox.ru>
2023-06-25 00:29:16 +0300

    update 3 files

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,11 +10,6 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
 
 public final class App implements Callable {
-//    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-//    private boolean version;
-//
-//    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-//    private boolean help;
 
     @Option(names = { "-f", "--format" }, description = "output format [default: stylish]", defaultValue = "stylish")
     private static String format;
